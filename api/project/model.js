51a8275eb3f8c7a43b5e2a4bb8a7bfa196a2185f
Stephen Aranda <aranda.stephen88@gmail.com>
2021-06-27 21:31:10 -0500

    model for project completed, router for project completed

--- api/project/model.js
+++ api/project/model.js
@@ -1,1 +1,55 @@
 // build your `Project` model here
+const db = require('../../data/dbConfig')
+
+async function getProjects(){
+    const retrieval = await db('projects')
+    .select('project_id',
+    'project_name',
+    'project_description',
+    'project_completed')
+    .orderBy('project_id','ASC')
+
+    const formatData=[]
+
+    retrieval.forEach(project=>{
+        formatData.push([{
+            project_id:project.project_id,
+            project_name:project.project_name,
+            project_description:project.project_description,
+            project_completed:project.project_completed ? true:false
+        }])
+    })
+    return formatData
+}
+
+ async function findProjectById(id){
+     const retrieval = await db('projects')
+     .select('project_id',
+             'project_name',
+             'project_description',
+             'project_completed')
+     .where('id',id)
+     
+     const formatData=[{
+         project_id:retrieval.project_id,
+         project_name:retrieval.project_name,
+         project_description:retrieval.project_description,
+         project_completed:retrieval.project_completed ? true:false
+     }]
+
+     return formatData
+ }
+
+
+ function addProject(project){
+    return db('projects')
+           .insert(project)
+           .then(([id])=>findProjectById(id))
+}
+
+module.exports={
+    getProjects,
+    addProject
+}
+
+
--- api/project/router.js
+++ api/project/router.js
@@ -1,1 +1,25 @@
 // build your `/api/projects` router here
+const Project = require('./model')
+const express = require('express')
+
+const router = express.Router()
+
+router.get('/',(req,res,next)=>{
+    Project.getProjects()
+    .then(projects=>{
+        res.status(200).json(projects)
+    })
+    .catch(next)
+})
+
+router.post('/',(req,res,next)=>{
+    const project = req.body
+
+    Project.addProject(project)
+    .then(postProject =>{
+        res.status(201).json(postProject)
+    })
+    .catch(next)
+})
+
+module.export = router
